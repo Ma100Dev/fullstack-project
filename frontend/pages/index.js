2070b59c12428de7ee90e09a2698970729ed466f
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-08-01 17:56:36 +0300

    Init frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div className="container">
+      hi
+    </div>
+  )
+}
+
+export default Home
