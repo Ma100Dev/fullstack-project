540bae2fc3701b3d0cdeeb24d1eddb6ae020109d
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-08-01 17:46:51 +0300

    Fix

--- backend/controllers/properties.js
+++ backend/controllers/properties.js
@@ -1,11 +1,12 @@
 const propertyRouter = require('express').Router();
 const Property = require('../models/property');
+const User = require('../models/user');
 require('express-async-errors');
 const { userExtractor } = require('../utils/middleware');
 
 propertyRouter.post('/', userExtractor, async (request, response) => {
     const { body } = request;
-    const user = new Property({
+    const property = new Property({
         address: body.address,
         price: Number(body.price),
         beds: Number(body.beds),
@@ -13,8 +14,9 @@ propertyRouter.post('/', userExtractor, async (request, response) => {
         petsAllowed: body.petsAllowed === 'true',
         owner: request.user.id,
     });
-    const savedUser = await user.save();
-    response.json(savedUser);
+    await User.findOneAndUpdate({ _id: request.user.id }, { $push: { properties: property.id } });
+    const saved = await property.save();
+    response.json(saved);
 });
 
 propertyRouter.get('/', async (request, response) => {
