f6dc890e8b4695bfcbb2892ea685cc387a6f757a
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-08-03 11:59:07 +0300

    Initialize Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,27 @@
+const initialState = null
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+    case 'SET_USER':
+        return action.user
+    case 'CLEAR_USER':
+        return initialState
+    default:
+        return state
+    }
+}
+
+export const setUser = (user) => {
+    return {
+        type: 'SET_USER',
+        user
+    }
+}
+
+export const clearUser = () => {
+    return {
+        type: 'CLEAR_USER'
+    }
+}
+
+export default reducer
