dd0b6b42a54ad19886af6a9ace5ab33b9994517c
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-10-25 19:44:21 +0300

    More backend updates for messages

--- backend/controllers/messages.js
+++ backend/controllers/messages.js
@@ -1,38 +1,25 @@
 const messageRouter = require('express').Router();
 const Message = require('../models/message');
-const Property = require('../models/property');
+const Conversation = require('../models/conversation');
 require('express-async-errors');
 const { userExtractor } = require('../utils/middleware');
 
-messageRouter.post('/', userExtractor, async (request, response) => { // This is completely broken. I need to fix it.
+messageRouter.post('/', userExtractor, async (request, response) => {
     const { body } = request;
-    const property = await Property.findById(body.property).populate('owner');
-    if (!property) {
-      return response.status(400).json({ error: 'Receiver does not exist' });
-    }
-    let receiver;
-    if (receiver._id.toString() === request.user._id.toString()) {
-      receiver = null; // TODO: This is a hack, fix it
-    } else {
-      receiver = property.owner;
-    }
+    const conversation = await Conversation.findById(body.conversation).populate('property')
+      .populate('starter')
+      .populate('receiver');
+    const receiver = conversation.starter.id === request.user.id
+      ? conversation.receiver.id : conversation.starter.id;
+
     const message = new Message({
         content: body.content,
         sender: request.user.id,
         receiver: receiver.id,
-        property: body.property,
+        conversation: conversation.id,
     });
     const saved = await message.save();
     response.json(saved);
-});
-
-messageRouter.get('/', userExtractor, async (request, response) => {
-    response.json(
-      await Message.find({ $or: [{ receiver: request.user.id }, { sender: request.user.id }] })
-        .populate('sender', { username: 1, name: 1 })
-        .populate('receiver', { username: 1, name: 1 })
-        .populate('property', { title: 1 }),
-    );
 });
 
 module.exports = messageRouter;
