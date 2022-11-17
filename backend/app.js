707cb8c50a447e48d1dc3edebd03a6135bdf4211
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-11-17 20:32:40 +0200

    Fix main logic

--- backend/app.js
+++ backend/app.js
@@ -25,45 +25,48 @@ if (!config.NO_RUN) {
   cryptoRouter = require('./controllers/crypto');
 }
 
-logger.log('Environment:', config.ENV);
+const createApp = async () => {
+    logger.log('Environment:', config.ENV);
 
-const app = express();
-if (config.ENV === 'development' || config.ENV === 'test') {
-  const { init } = require('./tests/db');
-  const { populateUsers } = require('./tests/populateDb');
-  init().then(() => {
-      logger.log('Connected to database');
-  });
-  if (config.POPULATE_DB) {
-    const populate = async () => {
-        const users = await populateUsers(10000);
-        logger.log('Populated database with users (', users.length, ')');
-    };
-    populate();
-  }
-} else {
-  const mongoUrl = config.MONGODB_URI;
-  logger.log(`Connecting to ${mongoUrl}`);
-  mongoose.connect(mongoUrl).then(() => {
-      logger.log('Connected to MongoDB');
-  }).catch((error) => {
-      logger.error(`Error connection to MongoDB: ${error.message}`);
-  });
-}
+    const app = express();
+    if (config.ENV === 'development' || config.ENV === 'test') { // If test or development, use in-memory database
+      const { init } = require('./tests/db');
+      const { populateUsers } = require('./tests/populateDb');
+      init().then(() => {
+          logger.log('Connected to database');
+      });
+      if (config.POPULATE_DB) { // If POPULATE_DB is true, populate the database with test data
+        const populate = async () => {
+            const users = await populateUsers(10000);
+            logger.log('Populated database with users (', users.length, ')');
+        };
+        await populate();
+      }
+    } else { // If production, use MongoDB
+      const mongoUrl = config.MONGODB_URI;
+      logger.log(`Connecting to ${mongoUrl}`);
+      mongoose.connect(mongoUrl).then(() => {
+          logger.log('Connected to MongoDB');
+      }).catch((error) => {
+          logger.error(`Error connection to MongoDB: ${error.message}`);
+      });
+    }
 
-if (config.NO_RUN) {
-  process.exit();
-}
+    if (config.NO_RUN) { // If NO_RUN is true, don't run the server and exit
+      process.exit();
+    }
 
-app.use(cors());
-app.use(express.json());
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/properties', propertyRouter);
-app.use('/messages', messageRouter);
-app.use('/conversations', conversationRouter);
-app.use('/crypto', cryptoRouter);
+    app.use(cors());
+    app.use(express.json());
+    app.use('/users', usersRouter);
+    app.use('/login', loginRouter);
+    app.use('/properties', propertyRouter);
+    app.use('/messages', messageRouter);
+    app.use('/conversations', conversationRouter);
+    app.use('/crypto', cryptoRouter);
 
-app.use(middleware.errorHandler);
+    app.use(middleware.errorHandler);
 
-module.exports = app;
+    return app;
+};
+module.exports = createApp;
