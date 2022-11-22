c62fe45c07b8ea58eed4d62c9e75fea148141875
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-11-22 14:37:21 +0200

    Add page selector to frontend

---
+++ frontend/src/components/PageSelector.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box } from '@mui/material';
+
+const PageSelector = ({ page, setPage, pageCount }) => {
+    const pages = Array.from({ length: pageCount }, (_, i) => i + 1);
+    return (
+        <Box className="page-selector" sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '1rem',
+        }}>
+            {pages.map(p => (
+                <button
+                    key={p}
+                    style={{
+                        backgroundColor: p === page ? 'black' : 'white',
+                        color: p === page ? 'white' : 'black',
+                        border: '1px solid black',
+                        padding: '0.5rem',
+                        margin: '0.5rem',
+                    }}
+                    onClick={() => setPage(p)}
+                >
+                    {p}
+                </button>
+            ))}
+        </Box>
+    );
+};
+
+PageSelector.propTypes = {
+    page: PropTypes.number.isRequired,
+    setPage: PropTypes.func.isRequired,
+    pageCount: PropTypes.number.isRequired,
+};
+
+export default PageSelector;
--- frontend/src/components/RentPage/index.jsx
+++ frontend/src/components/RentPage/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Rental from './Rental';
 import useRentals from '../../hooks/useRentals';
+import PageSelector from '../PageSelector';
 
 const RentPage = () => {
     const rentals = useRentals();
+    const [page, setPage] = React.useState(1);
     return (
         <>
             {rentals.map(rental => (
                 <Rental rental={rental} key={rental.id} />
             ))}
+            < PageSelector page={page} setPage={setPage} pageCount={5 /* TO BE REPLACED */} />
         </>
     );
 };
