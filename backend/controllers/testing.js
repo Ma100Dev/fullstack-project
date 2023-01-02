ddd731701d7c05ddefb1affe1397d8084ccbbc70
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2023-01-02 18:59:52 +0200

    Refactor e2e testing workflow

--- backend/controllers/testing.js
+++ backend/controllers/testing.js
@@ -2,6 +2,10 @@ const testingRouter = require('express').Router();
 const { clear } = require('../tests/utils/db');
 const { generateDefaultUser } = require('../tests/utils/populateDb');
 
+testingRouter.get('/ping', (request, response) => {
+    response.status(200).send('pong');
+});
+
 testingRouter.post('/reset', async (request, response) => {
     await clear();
     response.status(204).end();
