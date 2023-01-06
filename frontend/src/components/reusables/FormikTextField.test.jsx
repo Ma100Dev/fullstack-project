b80289c750367820c10b95b5974004a41ce59e38
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2023-01-06 16:43:43 +0200

    Complete FormikTextField.test.jsx

--- frontend/src/components/reusables/FormikTextField.test.jsx
+++ frontend/src/components/reusables/FormikTextField.test.jsx
@@ -19,3 +19,30 @@ test('FormikTextField renders with value', () => {
     expect(screen.getByPlaceholderText('Name')).not.toBeNull();
     expect(screen.getByDisplayValue('John')).not.toBeNull();
 });
+
+test('FormikTextField calls handleChange', async () => {
+    const inputs = [];
+    const name = [];
+    const handleChange = jest.fn((event) => {
+        inputs.push(event.target.value);
+        name.push(event.target.name);
+    });
+    render(<FormikTextField label="name" errors={{}} handleChange={handleChange} handleBlur={() => null} values={{}} type="text" touched={{}} placeholder="Name" />);
+    const user = userEvent.setup();
+    const input = screen.getByPlaceholderText('Name');
+    await user.type(input, 'John');
+    expect(handleChange).toHaveBeenCalledTimes(4);
+    expect(inputs).toEqual(['J', 'Jo', 'Joh', 'John']);
+    expect(name).toEqual(['name', 'name', 'name', 'name']);
+});
+
+test('FormikTextField calls handleBlur', async () => {
+    const handleBlur = jest.fn();
+    render(<FormikTextField label="name" errors={{}} handleChange={() => null} handleBlur={handleBlur} values={{}} type="text" touched={{}} placeholder="Name" />);
+    const input = screen.getByPlaceholderText('Name');
+    const user = userEvent.setup();
+    await user.click(input);
+    await user.tab();
+    expect(handleBlur).toHaveBeenCalledTimes(1);
+    expect(handleBlur.mock.calls[0][0].target.name).toBe('name');
+});
